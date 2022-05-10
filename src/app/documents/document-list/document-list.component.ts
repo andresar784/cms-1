ffcdfe9253b93b8373c984c5bb3d10d9ba7aae9d
Andres Rodriguez <rod16015@byui.edu>
2022-05-10 19:20:28 -0300

    week04 complete

--- src/app/documents/document-list/document-list.component.ts
+++ src/app/documents/document-list/document-list.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, EventEmitter, OnInit, Output } from '@angular/core';
+import { Document } from '../document.model';
 
 @Component({
   selector: 'cms-document-list',
@@ -6,10 +7,21 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./document-list.component.css']
 })
 export class DocumentListComponent implements OnInit {
+  @Output()  selectedDocumentEvent = new EventEmitter<Document>();
+
+  documents: Document[] = [
+    new Document(1, 'WDD 430 - Web fullstack development', 'Description of the WDD 430 class', 'https://byui.instructure.com/courses/85726/assignments/syllabus', ''),
+    new Document(2, 'WDD 360 - The fullstack development class. ','The old class','https://content.byui.edu/file/fb4c2d20-04c3-463d-93f6-725064a8b15d/4/fdmat108syllabus.html', ''),
+    new Document(3, 'MATH 100B','Beggining Algebra','https://content.byui.edu/file/fb4c2d20-04c3-463d-93f6-725064a8b15d/4/fdmat108syllabus.html', ''), 
+    new Document(4, 'Math108','Maths','https://content.byui.edu/file/fb4c2d20-04c3-463d-93f6-725064a8b15d/4/fdmat108syllabus.html', '')  
+  ];
 
   constructor() { }
 
   ngOnInit(): void {
+  }
+  onSelectedDocument(document: Document){
+    this.selectedDocumentEvent.emit(document); 
   }
 
 }
--- src/app/header.component.ts
+++ src/app/header.component.ts
@@ -6,6 +6,7 @@ import { Component, EventEmitter, OnInit, Output } from '@angular/core';
   styleUrls: ['./header.component.css']
 })
 export class HeaderComponent implements OnInit {
+  
   @Output() selectedFeatureEvent = new EventEmitter<string>();
 
   constructor() { }
